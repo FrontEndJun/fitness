fd26437892dba8ee978f11f16019275aecf62f99
Гоша <losetown343@e1.ru>
2019-12-21 22:25:31 +0500

    промо блок, акции+табы тренеры+слайдер

--- build/js/script.js
+++ build/js/script.js
@@ -1,47 +1,91 @@
 'use strict';
 
 (function() {
-  class Tabs {
-    constructor(node, content) {
-      this.node = node;
-      this.tabs = [].slice.call(node.querySelectorAll('li:not(.tabs__bar)'));
-      this.activeTab =
-        this.tabs.findIndex(function(elm) {
-          return elm.classList.contains('active');
-        }) || 0;
-      this.switcher = {
-        elm: node.querySelector('.tabs__bar'),
-        line: node.querySelector('.tabs__bar-line')
-      };
-      this.content = content;
-      this.openTab(this.activeTab);
-      this.node.addEventListener(
-        'click',
-        function(e) {
-          const index = this.tabs.indexOf(e.target.closest('li'));
-          this.openTab(index);
-          this.activeTab = index;
-        }.bind(this)
-      );
-    }
-
-    openTab(index) {
-      this.node.children[this.activeTab].classList.remove('active');
-      var tab = this.node.children[index];
-      tab.classList.add('active');
-      this.switcher.line.style.width = tab.offsetWidth + 'px';
-      this.switcher.line.style.left =
-        tab.getBoundingClientRect().left -
-        this.switcher.elm.getBoundingClientRect().left +
-        'px';
-
-      this.content.children[this.activeTab].classList.remove('active');
-      this.content.children[index].classList.add('active');
-    }
+  function Tabs(node, content) {
+    this.node = node;
+    this.tabs = [].slice.call(node.querySelectorAll('li:not(.tabs__bar)'));
+    this.activeTab =
+      this.tabs.findIndex(function(elm) {
+        return elm.classList.contains('active');
+      }) || 0;
+    this.switcher = {
+      elm: node.querySelector('.tabs__bar'),
+      line: node.querySelector('.tabs__bar-line')
+    };
+    this.content = content;
+    this.openTab(this.activeTab);
+    this.node.addEventListener(
+      'click',
+      function(e) {
+        const index = this.tabs.indexOf(e.target.closest('li'));
+        this.openTab(index);
+        this.activeTab = index;
+      }.bind(this)
+    );
   }
+  Tabs.prototype.openTab = function(index) {
+    this.node.children[this.activeTab].classList.remove('active');
+    var tab = this.node.children[index];
+    tab.classList.add('active');
+    this.switcher.line.style.width = tab.offsetWidth + 'px';
+    this.switcher.line.style.left =
+      tab.getBoundingClientRect().left -
+      this.switcher.elm.getBoundingClientRect().left +
+      'px';
+
+    this.content.children[this.activeTab].classList.remove('active');
+    this.content.children[index].classList.add('active');
+  };
+
+  function Slider(sliderNode, slidesToShow) {
+    this.slides = sliderNode.querySelectorAll('.slide');
+    this.curSlide = 0;
+    this.slidesToShow = slidesToShow || 1;
+    this.controls = {
+      prev: sliderNode.querySelector('button.prev'),
+      next: sliderNode.querySelector('button.next')
+    };
+    this.controls.prev.onclick = this.prevSlide.bind(this);
+    this.controls.next.onclick = this.nextSlide.bind(this);
+    this.show();
+  }
+  Slider.prototype.prevSlide = function() {
+    var sl = this.curSlide - this.slidesToShow;
+    this.curSlide = sl < 0 ? 0 : sl;
+    this.show();
+  };
+  Slider.prototype.nextSlide = function() {
+    var sl = this.curSlide + this.slidesToShow;
+    var l = this.slides.length;
+    this.curSlide = sl >= l ? this.curSlide : sl;
+    this.show();
+  };
+  Slider.prototype.show = function() {
+    for (var i = 0; i < this.slides.length; i++) {
+      var cond = i >= this.curSlide && i < this.curSlide + this.slidesToShow;
+      if (cond) {
+        this.slides[i].classList.add('active');
+      } else {
+        this.slides[i].classList.remove('active');
+      }
+    }
+  };
 
   const tabs = document.querySelector('.abonements__tabs');
   const content = document.querySelector('.abonements__list');
 
   new Tabs(tabs, content);
+  var tSlider = new Slider(document.querySelector('.slider'), 4);
+  adapSlides();
+
+  function adapSlides() {
+    if (document.documentElement.clientWidth >= 1200) {
+      tSlider.slidesToShow = 4;
+    } else if (document.documentElement.clientWidth < 767) {
+      tSlider.slidesToShow = 1;
+    } else {
+      tSlider.slidesToShow = 2;
+    }
+    tSlider.show();
+  }
 })();
