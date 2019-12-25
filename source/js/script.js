3afa514686e39ac01733fa1de8f9324c5c458df7
Гоша <losetown343@e1.ru>
2019-12-25 23:06:09 +0500

    fix issues

--- source/js/script.js
+++ source/js/script.js
@@ -23,14 +23,13 @@
       this.tabs.findIndex(function (elm) {
         return elm.classList.contains('active');
       }) || 0;
-    this.switcher = {
-      elm: node.querySelector('.tabs__bar'),
-      line: node.querySelector('.tabs__bar-line')
-    };
     this.content = content;
     this.openTab(this.activeTab);
     this.node.addEventListener('click', function (e) {
       var index = this.tabs.indexOf(e.target.closest('li'));
+      if (index === -1) {
+        return;
+      }
       this.openTab(index);
       this.activeTab = index;
     }.bind(this)
@@ -40,14 +39,7 @@
     this.node.children[this.activeTab].classList.remove('active');
     var tab = this.node.children[index];
     tab.classList.add('active');
-    this.switcher.line.style.width = tab.offsetWidth + 'px';
-    this.switcher.line.style.left =
-      tab.getBoundingClientRect().left -
-      this.switcher.elm.getBoundingClientRect().left +
-      'px';
 
-    this.content.children[this.activeTab].classList.remove('active');
-    this.content.children[index].classList.add('active');
   };
 
   function Slider(sliderNode, slidesToShow) {
